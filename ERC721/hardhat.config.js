bd0b4da2c8d76aa480a04d9134be131a04632d1a
Nicholas Chai <nicholas.chai@outlook.com>
2022-04-29 14:56:53 +0800

    just restructuring the ERC721 folders to be consistent with the other folders

--- ERC721/hardhat.config.js
+++ ERC721/hardhat.config.js
@@ -1,10 +1,28 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
 module.exports = {
   solidity: "0.8.4",
   paths: {
-    artifacts: "./backend/artifacts",
-    sources: "./backend/contracts",
-    cache: "./backend/cache",
-  }
+    artifacts: "./artifacts",
+    sources: "./contracts",
+    cache: "./cache",
+  },
+  networks: {
+    matic: {
+      url: "https://polygon-rpc.com",
+      accounts: [process.env.PRIVATE_KEY],
+      gas: "auto", 
+      gasPrice: "auto"
+    },
+    ethereum: {
+      url: "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  },
 };
